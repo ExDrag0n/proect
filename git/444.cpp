7c6d431ea37748402db9a66f0574f9e41b50e60e
ExDrag0n <igoreha.golkin@gmail.com>
2021-05-24 00:13:02 +0300

    f_c

---
+++ git/444.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.30804.86
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "444", "444\444.vcxproj", "{A33B54FA-B54C-4809-8E66-A511F448CB1A}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{A33B54FA-B54C-4809-8E66-A511F448CB1A}.Debug|x64.ActiveCfg = Debug|x64
+		{A33B54FA-B54C-4809-8E66-A511F448CB1A}.Debug|x64.Build.0 = Debug|x64
+		{A33B54FA-B54C-4809-8E66-A511F448CB1A}.Debug|x86.ActiveCfg = Debug|Win32
+		{A33B54FA-B54C-4809-8E66-A511F448CB1A}.Debug|x86.Build.0 = Debug|Win32
+		{A33B54FA-B54C-4809-8E66-A511F448CB1A}.Release|x64.ActiveCfg = Release|x64
+		{A33B54FA-B54C-4809-8E66-A511F448CB1A}.Release|x64.Build.0 = Release|x64
+		{A33B54FA-B54C-4809-8E66-A511F448CB1A}.Release|x86.ActiveCfg = Release|Win32
+		{A33B54FA-B54C-4809-8E66-A511F448CB1A}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {A698EB5D-1047-4284-865A-C3C9097919F9}
+	EndGlobalSection
+EndGlobal
